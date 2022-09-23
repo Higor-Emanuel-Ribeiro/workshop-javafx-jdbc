f68adac7fa3a1be80ebb13b16f3a48a499500639
Higor Emanuel <higoremanuel.r@gmail.com>
2022-09-23 10:37:26 -0300

    Main view

--- src/workshop/javafx/jdbc/Program.java
+++ src/workshop/javafx/jdbc/Program.java
@@ -2,17 +2,20 @@ package workshop.javafx.jdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Program extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        ScrollPane scrollPane = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        
+        Scene scene = new Scene(scrollPane);
         
         stage.setScene(scene);
         stage.show();
